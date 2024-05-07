f16b0d719803bc8929736df71e0b4f3b2a00eff2
Jordan Torske <jordan.torske@ucalgary.ca>
2024-05-06 22:45:57 -0600

    About me page draft finished

--- my-website/src/pages/About.js
+++ my-website/src/pages/About.js
@@ -1,11 +1,22 @@
 import './About.css'
+import me from '../images/me.png';
+
 function About() {
     return (
         <div className="about-container">
             <h1 className="about-title">About</h1>
             <div className="content-boxes">
-                <div className="content-box">Content Box 1</div>
-                <div className="content-box">Content Box 2</div>
+
+                <div className="content-box1">
+                <img src={me} alt="Placeholder" /></div>
+                <div className="content-box2">
+                <p>I'm a third year Software Engineering student at the University of Calgary with a passion in
+                    front-end development. I have experience in numerous technologies such as Figma, React and Angular, and have a deep understanding
+                    of HTML/CSS, SASS, JavaScript and TypeScript. <br></br><br></br>Besides front-end development, I love solving problems and challenging myself as an engineer, and im always
+                    open learning new things. Through my coursework, I've become proficient in numerous languages such as C/C++, Java and Python, and I have areas of interest
+                    in cybersecurity as well as machine learning. <br></br><br></br>When I'm not at my computer, I enjoy getting outside to go skiing, hiking or play soccer with my friends.
+                     </p>
+                </div>
             </div>
         </div>
     );
